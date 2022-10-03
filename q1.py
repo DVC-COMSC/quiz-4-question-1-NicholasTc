a562f4dd34dbc4888894b416cd2c623cb3b13573
Nicholas <nicholastc14@gmail.com>
2022-10-02 22:11:09 -0700

    Quiz 1

--- q1.py
+++ q1.py
@@ -2,7 +2,23 @@
 # ******************************
 # Make your Code
 # ******************************
+cluster_count = 0
+even_count = 0
 
+for i in range(10):
+    num = int(input())
+
+    # checks if the number is even
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        even_count = 0
+    
+    # adding the cluster
+    if even_count == 2:
+        cluster_count += 1
+
+print(cluster_count)
 
 # Requirement
 # No need to use list
